3ee826fd4c21e498bbf5d1f5701e9c6d96d56e1f
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-08-27 18:12:38 +0530

    feat: add MongoDB connection and update dependencies

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import healthRoutes from './routes/health.js';
+import connectDB from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(healthRoutes);
 
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
